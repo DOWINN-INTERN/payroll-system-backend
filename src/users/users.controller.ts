5c236324794f07cd9e6cd09a9f1bc8df63655d8d
jepnoda <devnoda@outlook.com>
2024-05-24 13:32:02 +0800

    Removed user's public endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Public } from 'src/common/decorators';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Public()
   @Get()
   findAll() {
     return this.usersService.findAll();
